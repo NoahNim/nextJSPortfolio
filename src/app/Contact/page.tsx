8cfc8e06e274ea68cda5616b95ed614fe17adadb
Noah Medoff <noahmedoff@gmail.com>
2023-11-17 15:41:15 -0800

    change file structure for pages

---
+++ src/app/Contact/page.tsx
@@ -0,0 +1,14 @@
+import NavPageBar from "../Navigation/page"
+
+export default function Page() {
+    return (
+        <div>
+            <div className="flex justify-center">
+                <NavPageBar />
+            </div>
+            <div>
+                Under construction...
+            </div>
+        </div>
+    )
+}
